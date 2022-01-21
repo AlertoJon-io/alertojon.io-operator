08406196a1a9f505c00531671391a3fb6e65ffcd
pick jonathan <pickjonathan@gmail.com>
2022-01-21 15:20:21 +0200

    update controller with permissions to create update and delete deployments and secrets

--- controllers/pagerduty_controller.go
+++ controllers/pagerduty_controller.go
@@ -39,6 +39,9 @@ type PagerdutyReconciler struct {
 //+kubebuilder:rbac:groups=alertprovider.alertojon.io,resources=pagerduties/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=alertprovider.alertojon.io,resources=pagerduties/finalizers,verbs=update
 
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
